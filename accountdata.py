253229245d46f204ff1c457c7e0038de1ca4538e
Daniel Weikert <weikert-deluxe@gmx.de>
2017-06-13 18:29:20 +0200

    Add files via upload

---
+++ accountdata.py
@@ -0,0 +1,8 @@
+class Accessdata:
+    def __init__(self):
+
+        self.app_key='Enter your key here'
+        self.app_secret='Enter your secret here'
+
+        self.access_token = 'Enter your token here'
+        self.access_secret = 'Enter your token secret here'
